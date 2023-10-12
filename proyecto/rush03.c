98dd0040ba5283d55e54028b32be735eec52d57e
dani-mg-05 <danimg0502@gmail.com>
2023-10-12 21:52:52 +0200

    Rush00

---
+++ proyecto/rush03.c
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   rush02.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: adpedrer <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/09 17:07:45 by adpedrer          #+#    #+#             */
+/*   Updated: 2023/07/09 17:47:19 by adpedrer         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	ft_putchar(char c);
+void	printer(int x, char a, char b, char c);
+
+void	rush(int x, int y)
+{
+	int	c1;
+
+	c1 = 0;
+	if (x >= 1 && y >= 1)
+	{
+		while (c1 <= y - 1)
+		{
+			if (c1 == 0)
+				printer(x, 'A', 'B', 'C');
+			else if (c1 == y - 1)
+				printer(x, 'A', 'B', 'C');
+			else
+				printer(x, 'B', ' ', 'B');
+			c1++;
+		}
+	}
+}
+
+void	printer(int x, char a, char b, char c)
+{
+	int	c2;
+
+	c2 = 0;
+	while (c2 <= x - 1)
+	{
+		if (c2 == 0)
+			ft_putchar(a);
+		else if (c2 == x - 1)
+			ft_putchar(c);
+		else
+			ft_putchar(b);
+			c2++;
+	}
+	ft_putchar('\n');
+}
